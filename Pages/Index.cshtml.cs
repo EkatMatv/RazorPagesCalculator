fba84fc1a7f690c93a978e1e7353fe969cc7a31c
Ekat Matv <ekatmatv@example.com>
2025-11-06 22:53:06 +0300

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesCalculator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public double FirstNumber { get; set; }
+
+        [BindProperty]
+        public double SecondNumber { get; set; }
+
+        [BindProperty]
+        public string Operation { get; set; } = "add";
+
+        public string Result { get; set; } = "";
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            Result = Operation switch
+            {
+                "add" => $"{FirstNumber} + {SecondNumber} = {FirstNumber + SecondNumber}",
+                "subtract" => $"{FirstNumber} - {SecondNumber} = {FirstNumber - SecondNumber}",
+                "multiply" => $"{FirstNumber} × {SecondNumber} = {FirstNumber * SecondNumber}",
+                "divide" => SecondNumber != 0 ? $"{FirstNumber} ÷ {SecondNumber} = {FirstNumber / SecondNumber}" : "Ошибка: деление на ноль",
+                _ => "Неизвестная операция"
+            };
+        }
+    }
+}
